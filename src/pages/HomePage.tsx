7ae4dffad3b4cf08c7afc7b4318e3f8718c160fb
softwarecalc-dev <info.softwarecalc@gmail.com>
2026-04-14 20:35:29 +0200

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -66,7 +66,7 @@ const otherTools = TOOLS
 
   <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
 
-    Object.entries(CATEGORY_META)
+    {Object.entries(CATEGORY_META)
       .filter(([key]) =>
         TOOLS.some(tool => tool.category === key && tool.available)
       )
